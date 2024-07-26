a2a18889a52f9d3d0f74e606e6c732feb34b510f
Anup Prasad <134351497+anuppd@users.noreply.github.com>
2024-07-26 15:54:46 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const num1 = 5;
+const num2 = 10;
+
+if (num1 % 2 == 0) {
+    console.log(num1, "is an even number.");
+} else {
+    console.log(num1, "is an odd number.");
+}
+
+if (num2 % 2 == 0) {
+    console.log(num2, "is an even number.");
+} else {
+    console.log(num2, "is an odd number.");
+}
